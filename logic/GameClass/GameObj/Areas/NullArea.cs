80d8112c845c8534216e2f98103a9a50c1d0bb99
964293341 <asdawej123@163.com>
2023-11-01 12:53:47 +0800

    feat(Map): :sparkles: add NullArea

---
+++ logic/GameClass/GameObj/Areas/NullArea.cs
@@ -0,0 +1,13 @@
+﻿using Preparation.Utility;
+
+namespace GameClass.GameObj.Areas;
+
+public class NullArea : Immovable
+{
+    public override bool IsRigid => false;
+    public override ShapeType Shape => ShapeType.Null;
+    public NullArea(XY initPos)
+        : base(initPos, int.MaxValue, GameObjType.Null)
+    {
+    }
+}
