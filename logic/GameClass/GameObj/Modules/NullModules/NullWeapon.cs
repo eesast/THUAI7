41398c12601fb63220ee0333a887408345f305c2
964293341 <asdawej123@163.com>
2023-11-04 22:29:01 +0800

    feat: :sparkles: revise NullModules & NullOccupation to singleton

--- logic/GameClass/GameObj/Modules/NullModules/NullWeapon.cs
+++ logic/GameClass/GameObj/Modules/NullModules/NullWeapon.cs
@@ -5,6 +5,8 @@ namespace GameClass.GameObj.Modules;
 
 public class NullWeapon : IWeapon
 {
+    public static NullWeapon Instance { get; } = new();
     public BulletType BulletType => BulletType.Null;
     public int Cost => 0;
+    private NullWeapon() { }
 }
--- logic/GameClass/GameObj/Occupations/NullOccupation.cs
+++ logic/GameClass/GameObj/Occupations/NullOccupation.cs
@@ -4,7 +4,9 @@ namespace GameClass.GameObj.Occupations;
 
 public class NullOccupation : IOccupation
 {
+    public static NullOccupation Instance { get; } = new();
     public int MoveSpeed => 0;
     public int MaxHp => 0;
     public int ViewRange => 0;
+    private NullOccupation() { }
 }
