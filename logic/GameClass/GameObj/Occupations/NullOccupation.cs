7046da563a57164a8937eeb4e9ccaaafef8d797b
964293341 <asdawej123@163.com>
2023-11-04 22:49:53 +0800

    feat(Occupations): :sparkles: add interface method Cost & BaseArmor & BaseShield
    - add Cost for GameData

--- logic/GameClass/GameObj/Occupations/NullOccupation.cs
+++ logic/GameClass/GameObj/Occupations/NullOccupation.cs
@@ -8,5 +8,8 @@ public class NullOccupation : IOccupation
     public int MoveSpeed => 0;
     public int MaxHp => 0;
     public int ViewRange => 0;
+    public int Cost => 0;
+    public int BaseArmor => 0;
+    public int BaseShield => 0;
     private NullOccupation() { }
 }
