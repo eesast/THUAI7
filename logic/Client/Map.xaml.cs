a7f8c011676714fba1f6f72bbf74600ce4a02d7d
Grange <2634070476@qq.com>
2023-12-16 16:29:59 +0800

    fix: 🐛 build bug for mainpage & map

--- logic/Client/Map.xaml.cs
+++ logic/Client/Map.xaml.cs
@@ -12,32 +12,32 @@ public partial class Map : ContentView
         DrawMap();
     }
 
-        public void DrawMap()
+    public void DrawMap()
+    {
+        for (int i = 0; i < 50; i++)
         {
-            for (int i = 0; i < 50; i++)
+            InMapGrid.AddRowDefinition(new RowDefinition(13));
+            for (int j = 0; j < 50; j++)
             {
-                InMapGrid.AddRowDefinition(new RowDefinition(13));
-                for (int j = 0; j < 50; j++)
+                InMapGrid.AddColumnDefinition(new ColumnDefinition(13));
+                BoxView boxView = new BoxView
                 {
-                    InMapGrid.AddColumnDefinition(new ColumnDefinition(13));
-                    BoxView boxView = new BoxView
-                    {
-                        Color = Colors.Gray,
-                        HeightRequest = 13,
-                        WidthRequest = 13
-                    };
-                    Border border = new Border
-                    {
-                        Content = boxView,
-                        HeightRequest = 13,
-                        WidthRequest = 13,
-                        Padding = 0,
-                        StrokeThickness = 0.005
-                    };
-                    InMapGrid.Children.Add(border);
-                    InMapGrid.SetColumn(border, i);
-                    InMapGrid.SetRow(border, j);
-                }
+                    Color = Colors.Gray,
+                    HeightRequest = 13,
+                    WidthRequest = 13
+                };
+                Border border = new Border
+                {
+                    Content = boxView,
+                    HeightRequest = 13,
+                    WidthRequest = 13,
+                    Padding = 0,
+                    StrokeThickness = 0.005
+                };
+                InMapGrid.Children.Add(border);
+                InMapGrid.SetColumn(border, i);
+                InMapGrid.SetRow(border, j);
             }
         }
+    }
 }
