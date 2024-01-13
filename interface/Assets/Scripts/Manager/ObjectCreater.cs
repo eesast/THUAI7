538d54923fbd117b60a192aea8b8b92642743ed3
hhzzff <2070620600@qq.com>
2024-01-14 02:37:12 +0800

    feat: :sparkles: Added bloom effects to the spaceship and fortress.

--- interface/Assets/Scripts/Manager/ObjectCreater.cs
+++ interface/Assets/Scripts/Manager/ObjectCreater.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class ObjectCreater : SingletonMono<ObjectCreater>
 {
-    public GameObject CreateObject(GameObject targetG, Vector2 position, Quaternion quaternion, Transform targetPa, int teamKey = -1)
+    public GameObject CreateObject(GameObject targetG, Vector2 position, Quaternion quaternion, Transform targetPa, int teamKey = 0)
     {
         GameObject obj = Instantiate(targetG, position, quaternion, targetPa);
         if (targetG.transform.childCount == 0) return obj;
-        Tuple<Color, Color> col = RendererControl.GetInstance().GetColFromTeam(teamKey);
-        obj.transform.Find("mask1").GetComponent<SpriteRenderer>().material.color = col.Item1;
-        obj.transform.Find("mask2").GetComponent<SpriteRenderer>().material.color = col.Item2;
+        Debug.Log("step1");
+        RendererControl.GetInstance().SetColToChild(teamKey, obj.transform);
+        Debug.Log("step3");
+        // obj.transform.Find("mask1").GetComponent<Renderer>().GetPropertyBlock(CurrentPropertyBlock);
+        // Tuple<MaterialPropertyBlock, MaterialPropertyBlock> col = RendererControl.GetInstance().GetColFromTeam(teamKey, CurrentPropertyBlock);
+        // obj.transform.Find("mask1").GetComponent<Renderer>().SetPropertyBlock(col.Item1);
+        // obj.transform.Find("mask2").GetComponent<Renderer>().SetPropertyBlock(col.Item2);
         return obj;
     }
 }
