6939130d0e32394ee96df130b6d2f8ec05bd046a
sigureling <1678348940@qq.com>
2024-02-25 13:54:19 +0800

    Update PyAPI

---
+++ CAPI/python/PyAPI/API.py
@@ -0,0 +1,5 @@
+import PyAPI.structures as THUAI6
+from PyAPI.Interface import ILogic, IStudentAPI, ITrickerAPI, IGameTimer, IAI
+from math import pi
+from concurrent.futures import ThreadPoolExecutor, Future
+from typing import List, cast, Tuple, Union
