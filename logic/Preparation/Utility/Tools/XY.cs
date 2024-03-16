a7dd440a8d35edbb456d70698d59abcb90d2052c
964293341 <asdawej123@163.com>
2024-03-16 15:15:15 +0800

    feat(XY): :sparkles: implement XY (as GridXY) & CellXY

--- logic/Preparation/Utility/Tools/XY.cs
+++ logic/Preparation/Utility/Tools/XY.cs
@@ -2,10 +2,12 @@
 
 namespace Preparation.Utility;
 
-public class XY
+public readonly struct XY
 {
-    public int x;
-    public int y;
+    public readonly int x;
+    public readonly int y;
+
+    #region ctor
     public XY(int x, int y)
     {
         this.x = x;
@@ -29,46 +31,28 @@ public class XY
             this.y = (int)(length * Math.Sin(Direction.Angle()));
         }
     }
-    public override string ToString()
-    {
-        return "(" + x.ToString() + "," + y.ToString() + ")";
-    }
-    public static int operator *(XY v1, XY v2)
-    {
-        return (v1.x * v2.x) + (v1.y * v2.y);
-    }
-    public static XY operator *(int a, XY v2)
-    {
-        return new XY(a * v2.x, a * v2.y);
-    }
-    public static XY operator *(XY v2, int a)
-    {
-        return new XY(a * v2.x, a * v2.y);
-    }
-    public static XY operator +(XY v1, XY v2)
-    {
-        return new XY(v1.x + v2.x, v1.y + v2.y);
-    }
-    public static XY operator -(XY v1, XY v2)
-    {
-        return new XY(v1.x - v2.x, v1.y - v2.y);
-    }
-    public static bool operator ==(XY v1, XY v2)
-    {
-        return v1.x == v2.x && v1.y == v2.y;
-    }
-    public static bool operator !=(XY v1, XY v2)
-    {
-        return v1.x != v2.x || v1.y != v2.y;
-    }
+    #endregion
 
+    public override string ToString() => $"({x}, {y})";
+    /// <returns>数量积</returns>
+    public static int operator *(XY v1, XY v2) => (v1.x * v2.x) + (v1.y * v2.y);
+    /// <returns>左数乘</returns>
+    public static XY operator *(int a, XY v2) => new(a * v2.x, a * v2.y);
+    /// <returns>右数乘</returns>
+    public static XY operator *(XY v2, int a) => new(a * v2.x, a * v2.y);
+    public static XY operator +(XY v1, XY v2) => new(v1.x + v2.x, v1.y + v2.y);
+    public static XY operator -(XY v1, XY v2) => new(v1.x - v2.x, v1.y - v2.y);
+    public static bool operator ==(XY v1, XY v2) => (v1.x == v2.x) && (v1.y == v2.y);
+    public static bool operator !=(XY v1, XY v2) => (v1.x != v2.x) || (v1.y != v2.y);
+
+    #region Distance
     public static double DistanceFloor3(XY p1, XY p2)
     {
         long c = (((long)(p1.x - p2.x) * (p1.x - p2.x)) + ((long)(p1.y - p2.y) * (p1.y - p2.y))) * 1000000;
         long t = c / 2 + 1;
         while (t * t > c || (t + 1) * (t + 1) <= c)
             t = (c / t + t) / 2;
-        return (double)t / 1000.0;
+        return t / 1000.0;
     }
     public static double DistanceCeil3(XY p1, XY p2)
     {
@@ -76,42 +60,96 @@ public class XY
         long t = c / 2 + 1;
         while (t * t > c || (t + 1) * (t + 1) <= c)
             t = (c / t + t) / 2;
-        if (t * t == c) return (double)t / 1000.0;
-        else return (double)(t + 1) / 1000.0;
+        if (t * t == c) return t / 1000.0;
+        else return (t + 1) / 1000.0;
     }
-    public double Length()
-    {
-        return Math.Sqrt(((long)x * x) + ((long)y * y));
-    }
-    public double Angle()
-    {
-        return Math.Atan2(y, x);
-    }
+    #endregion
+
+    public double Length() => Math.Sqrt(((long)x * x) + ((long)y * y));
+    public double Angle() => Math.Atan2(y, x);
 
     /// <summary>
     /// 逆时针旋转90°
     /// </summary>
-    public XY Perpendicular()
-    {
-        return new XY(-y, x);
-    }
+    public XY Perpendicular() => new(-y, x);
 
-    public override bool Equals(object? obj) => obj is not null && obj is XY xy && this == xy;
+    public override bool Equals(object? obj) => (obj is not null) && (obj is XY xy) && (this == xy);
 
-    public override int GetHashCode()
+    public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode();
+}
+
+public readonly struct CellXY
+{
+    public readonly int x;
+    public readonly int y;
+
+    #region ctor
+    public CellXY(int x, int y)
     {
-        return this.x.GetHashCode() ^ this.y.GetHashCode();
+        this.x = x;
+        this.y = y;
     }
-}
-public class GridXY : XY
-{
-    public GridXY(int x, int y) : base(x, y) { }
-    public GridXY(double angle, double length) : base(angle, length) { }
-    public GridXY(XY Direction, double length) : base(Direction, length) { }
-}
-public class CellXY : XY
-{
-    public CellXY(int x, int y) : base(x, y) { }
-    public CellXY(double angle, double length) : base(angle, length) { }
-    public CellXY(XY Direction, double length) : base(Direction, length) { }
+    public CellXY(double angle, double length)
+    {
+        this.x = (int)(length * Math.Cos(angle));
+        this.y = (int)(length * Math.Sin(angle));
+    }
+    public CellXY(XY Direction, double length)
+    {
+        if (Direction.x == 0 && Direction.y == 0)
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        else
+        {
+            this.x = (int)(length * Math.Cos(Direction.Angle()));
+            this.y = (int)(length * Math.Sin(Direction.Angle()));
+        }
+    }
+    #endregion
+
+    public override string ToString() => $"({x}, {y})";
+    /// <returns>数量积</returns>
+    public static int operator *(CellXY v1, CellXY v2) => (v1.x * v2.x) + (v1.y * v2.y);
+    /// <returns>左数乘</returns>
+    public static CellXY operator *(int a, CellXY v2) => new(a * v2.x, a * v2.y);
+    /// <returns>右数乘</returns>
+    public static CellXY operator *(CellXY v2, int a) => new(a * v2.x, a * v2.y);
+    public static CellXY operator +(CellXY v1, CellXY v2) => new(v1.x + v2.x, v1.y + v2.y);
+    public static CellXY operator -(CellXY v1, CellXY v2) => new(v1.x - v2.x, v1.y - v2.y);
+    public static bool operator ==(CellXY v1, CellXY v2) => (v1.x == v2.x) && (v1.y == v2.y);
+    public static bool operator !=(CellXY v1, CellXY v2) => (v1.x != v2.x) || (v1.y != v2.y);
+
+    #region Distance
+    public static double DistanceFloor3(CellXY p1, CellXY p2)
+    {
+        long c = (((long)(p1.x - p2.x) * (p1.x - p2.x)) + ((long)(p1.y - p2.y) * (p1.y - p2.y))) * 1000000;
+        long t = c / 2 + 1;
+        while (t * t > c || (t + 1) * (t + 1) <= c)
+            t = (c / t + t) / 2;
+        return t / 1000.0;
+    }
+    public static double DistanceCeil3(CellXY p1, CellXY p2)
+    {
+        long c = (((long)(p1.x - p2.x) * (p1.x - p2.x)) + ((long)(p1.y - p2.y) * (p1.y - p2.y))) * 1000000;
+        long t = c / 2 + 1;
+        while (t * t > c || (t + 1) * (t + 1) <= c)
+            t = (c / t + t) / 2;
+        if (t * t == c) return t / 1000.0;
+        else return (t + 1) / 1000.0;
+    }
+    #endregion
+
+    public double Length() => Math.Sqrt(((long)x * x) + ((long)y * y));
+    public double Angle() => Math.Atan2(y, x);
+
+    /// <summary>
+    /// 逆时针旋转90°
+    /// </summary>
+    public CellXY Perpendicular() => new(-y, x);
+
+    public override bool Equals(object? obj) => (obj is not null) && (obj is CellXY xy) && (this == xy);
+
+    public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode();
 }
