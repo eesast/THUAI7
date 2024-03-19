4acc9b1e3823442f91b0038340305c360b7143d8
hhzzff <2070620600@qq.com>
2024-03-19 19:23:47 +0800

    feat: :sparkles: Added features such as building ships, moving, and attacking for local mode.

---
+++ interface/interface_local/Assets/Scripts/InteractBase/InteractControl.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractControl : Singleton<InteractControl>
+{
+    public enum InteractType
+    {
+        Base,
+        Ship,
+
+    }
+    public enum InteractOption
+    {
+        None,
+        BuildCivil,
+        BuildMilitary,
+        BuildFlag,
+        RecycleShip,
+        RecoverShip,
+        InstallModuleArmor1,
+        InstallModuleArmor2,
+        InstallModuleArmor3,
+        InstallModuleShield1,
+        InstallModuleShield2,
+        InstallModuleShield3,
+        InstallModuleProducer1,
+        InstallModuleProducer2,
+        InstallModuleProducer3,
+        InstallModuleConstructor1,
+        InstallModuleConstructor2,
+        InstallModuleConstructor3,
+        InstallModuleLaserGun,
+        InstallModulePlasmaGun,
+        InstallModuleShellGun,
+        InstallModuleMissileGun,
+        InstallModuleArcGun,
+
+    }
+    public readonly Dictionary<InteractType, List<InteractOption>> interactOptions = new Dictionary<InteractType, List<InteractOption>>(){
+        {InteractType.Base,
+            new List<InteractOption>{
+                InteractOption.BuildCivil,
+                InteractOption.BuildMilitary,
+                InteractOption.BuildFlag,
+                }},
+        {InteractType.Ship,
+            new List<InteractOption>{
+                InteractOption.RecycleShip,
+                InteractOption.RecoverShip,
+                InteractOption.InstallModuleArmor1,
+                InteractOption.InstallModuleArmor2,
+                InteractOption.InstallModuleArmor3,
+                InteractOption.InstallModuleShield1,
+                InteractOption.InstallModuleShield2,
+                InteractOption.InstallModuleShield3,
+                InteractOption.InstallModuleProducer1,
+                InteractOption.InstallModuleProducer2,
+                InteractOption.InstallModuleProducer3,
+                InteractOption.InstallModuleConstructor1,
+                InteractOption.InstallModuleConstructor2,
+                InteractOption.InstallModuleConstructor3,
+                InteractOption.InstallModuleLaserGun,
+                InteractOption.InstallModulePlasmaGun,
+                InteractOption.InstallModuleShellGun,
+                InteractOption.InstallModuleMissileGun,
+                InteractOption.InstallModuleArcGun,
+                }},
+    };
+}
---
+++ interface/interface_local/Assets/Scripts/ParaDefine.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParaDefine : SingletonMono<ParaDefine>
+{
+    public BulletData laserData, plasmaData, shellData, missileData, arcData;
+}
