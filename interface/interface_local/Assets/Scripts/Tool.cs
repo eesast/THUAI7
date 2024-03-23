f023bf33e82b690647cbd471dc620dd9ff398c14
hhzzff <2070620600@qq.com>
2024-03-23 23:30:44 +0800

    feat: :sparkles: Improved health ui

--- interface/interface_local/Assets/Scripts/Tool.cs
+++ interface/interface_local/Assets/Scripts/Tool.cs
@@ -9,4 +9,14 @@ public class Tool : Singleton<Tool>
     {
         return a.Next(min, max);
     }
+    public Vector2 GridToCell(Vector2 grid)
+    {
+        return new Vector2(50.5f - grid.y, 0.5f + grid.x);
+    }
+    public bool CheckBeside(Vector2 grid, Vector2 cell)
+    {
+        if (Mathf.Abs(GridToCell(grid).x - cell.x) + Mathf.Abs(GridToCell(grid).y - cell.y) <= 2)
+            return true;
+        return false;
+    }
 }
