2271d9009cc7db7205a1e3adf89d10ca9b0b7aad
ONLOX <3316820553@qq.com>
2024-03-30 23:58:33 +0800

    feat: add entry detection

--- installer/ViewModel/LaunchViewModel.cs
+++ installer/ViewModel/LaunchViewModel.cs
@@ -18,9 +18,18 @@ namespace installer.ViewModel
         {
             Downloader = downloader;
 
+            IP = Downloader.Data.Config.Commands.IP;
+            Port = Downloader.Data.Config.Commands.Port;
+            TeamID = Downloader.Data.Config.Commands.TeamID;
+            PlayerID = Downloader.Data.Config.Commands.PlayerID;
+            SweeperType = Downloader.Data.Config.Commands.SweeperType;
+            PlaybackFile = Downloader.Data.Config.Commands.PlaybackFile;
             PlaybackSpeed = Downloader.Data.Config.Commands.PlaybackSpeed.ToString();
+
+            SaveEnabled = true;
             StartEnabled = true;
 
+            SaveBtnClickedCommand = new AsyncRelayCommand(SaveBtnClicked);
             StartBtnClickedCommand = new AsyncRelayCommand(StartBtnClicked);
         }
 
@@ -36,62 +45,116 @@ namespace installer.ViewModel
         }
 
 
-        public string IP
+        private string? ip;
+        public string? IP
         {
-            get => Downloader.Data.Config.Commands.IP;
+            get => ip;
             set
             {
-                Downloader.Data.Config.Commands.IP = value;
+                ip = value;
+                if (ip == Downloader.Data.Config.Commands.IP)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
 
-        public string Port
+        private string? port;
+        public string? Port
         {
-            get => Downloader.Data.Config.Commands.Port;
+            get => port;
             set
             {
-                Downloader.Data.Config.Commands.Port = value;
+                port = value;
+                if (port == Downloader.Data.Config.Commands.Port)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
 
-        public string TeamID
+        private string? teamID;
+        public string? TeamID
         {
-            get => Downloader.Data.Config.Commands.TeamID;
+            get => teamID;
             set
             {
-                Downloader.Data.Config.Commands.TeamID = value;
+                teamID = value;
+                if (teamID == Downloader.Data.Config.Commands.TeamID)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
 
-        public string PlayerID
+        private string? playerID;
+        public string? PlayerID
         {
-            get => Downloader.Data.Config.Commands.PlayerID;
+            get => playerID;
             set
             {
-                Downloader.Data.Config.Commands.PlayerID = value;
+                playerID = value;
+                if (playerID == Downloader.Data.Config.Commands.PlayerID)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
 
-        public string SweeperType
+        private string? sweeperType;
+        public string? SweeperType
         {
-            get => Downloader.Data.Config.Commands.SweeperType;
+            get => sweeperType;
             set
             {
-                Downloader.Data.Config.Commands.SweeperType = value;
+                sweeperType = value;
+                if (sweeperType == Downloader.Data.Config.Commands.SweeperType)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
 
-        public string PlaybackFile
+        private string? playbackFile;
+        public string? PlaybackFile
         {
-            get => Downloader.Data.Config.Commands.PlaybackFile;
+            get => playbackFile;
             set
             {
-                Downloader.Data.Config.Commands.PlaybackFile = value;
+                playbackFile = value;
+                if (playbackFile == Downloader.Data.Config.Commands.PlaybackFile)
+                {
+                    StartEnabled = true;
+                }
+                else
+                {
+                    StartEnabled = false;
+                }
                 OnPropertyChanged();
             }
         }
@@ -103,17 +166,26 @@ namespace installer.ViewModel
             set
             {
                 playbackSpeed = value;
-                try
+                if (playbackSpeed == Downloader.Data.Config.Commands.PlaybackSpeed.ToString())
                 {
-                    Downloader.Data.Config.Commands.PlaybackSpeed = Convert.ToDouble(value);
-                    DebugAlert = null;
                     StartEnabled = true;
                 }
-                catch (Exception e)
+                else
                 {
-                    DebugAlert = e.ToString();
                     StartEnabled = false;
                 }
+                OnPropertyChanged();
+            }
+        }
+
+
+        private bool saveEnabled;
+        public bool SaveEnabled
+        {
+            get => saveEnabled;
+            set
+            {
+                saveEnabled = value;
                 OnPropertyChanged();
             }
         }
@@ -121,6 +193,7 @@ namespace installer.ViewModel
         private bool startEnabled;
         public bool StartEnabled
         {
+
             get => startEnabled;
             set
             {
@@ -129,11 +202,135 @@ namespace installer.ViewModel
             }
         }
 
+        public ICommand SaveBtnClickedCommand { get; }
+        private async Task SaveBtnClicked()
+        {
+            DebugAlert = "Save";
+            var cntExp = await Task.Run(() => Save());
+            if (cntExp == 0)
+            {
+                StartEnabled = true;
+            }
+        }
 
         public ICommand StartBtnClickedCommand { get; }
         private async Task StartBtnClicked()
         {
             await Task.Run(() => Start());
+        }
+
+        private int Save()
+        {
+            int cntExp = 0;
+            Task.Run(() => {
+                try
+                {
+                    if (IP == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    Downloader.Data.Config.Commands.IP = IP;
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "IP: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    if (Port == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    Downloader.Data.Config.Commands.Port = Port;
+                }
+                catch (Exception e)
+                {
+                    if (Port == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    DebugAlert = "Port: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    if (TeamID == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    Downloader.Data.Config.Commands.TeamID = TeamID;
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "Team ID: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    if (PlayerID == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    Downloader.Data.Config.Commands.PlayerID = PlayerID;
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "Player ID: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    if (SweeperType == null)
+                    {
+                        throw new Exception("empty");
+                    }
+                    Downloader.Data.Config.Commands.SweeperType = SweeperType;
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "Sweeper Type: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    Downloader.Data.Config.Commands.PlaybackFile = PlaybackFile;
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "Playback File: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            Task.Run(() => {
+                try
+                {
+                    Downloader.Data.Config.Commands.PlaybackSpeed = Convert.ToDouble(PlaybackSpeed);
+                }
+                catch (Exception e)
+                {
+                    DebugAlert = "Playback Speed: " + e.Message;
+                    StartEnabled = false;
+                    cntExp++;
+                }
+            });
+            return cntExp;
         }
 
         private void Start()
