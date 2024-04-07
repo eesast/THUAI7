716327d62a48e78b253ee12bc4c5f88319cbb934
Xuc Pan <panxuc@panxuc.com>
2024-04-07 23:36:16 +0800

    fix: :bug: require playerid to build ship

--- logic/Server/RpcServices.cs
+++ logic/Server/RpcServices.cs
@@ -507,18 +507,6 @@ namespace Server
             Console.WriteLine($"TRY BuildShip: ShipType {request.ShipType} from Team {request.TeamId}");
 #endif
             BoolRes boolRes = new();
-            var ship = game.TeamList[(int)request.TeamId].ShipPool.Find(
-                (ship) => ship.PlayerID == request.PlayerId);
-            if (ship == null)
-            {
-                boolRes.ActSuccess = false;
-                return Task.FromResult(boolRes);
-            }
-            else if (ship.IsRemoved == false)
-            {
-                boolRes.ActSuccess = false;
-                return Task.FromResult(boolRes);
-            }
             boolRes.ActSuccess = game.ActivateShip(request.TeamId, Transformation.ShipTypeFromProto(request.ShipType), request.BirthpointIndex) != GameObj.invalidID;
 #if DEBUG
             Console.WriteLine("END BuildShip");
