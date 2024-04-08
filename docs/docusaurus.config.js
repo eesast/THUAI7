40ba230b5ffa034ceb2baf3b99a61c1abe1b6c7d
Xuc Pan <panxuc@panxuc.com>
2024-04-08 18:54:20 +0800

    ci: :memo: modify gh-pages path

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ module.exports = {
     url: 'https://eesast.github.io/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/THUAI7/docs/build/',
+    baseUrl: '/THUAI7/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
