dcdcd07c5cc08387f9fc8a4f091a9e106dc82211
964293341 <asdawej123@163.com>
2024-04-15 00:47:36 +0800

    feat(ShipManager): :sparkles: ship building cost will be deducted now

--- logic/Gaming/ShipManager.cs
+++ logic/Gaming/ShipManager.cs
@@ -44,6 +44,7 @@ namespace Gaming
                 {
                     return false;
                 }
+                game.TeamList[(int)ship.TeamID].MoneyPool.SubMoney(activateCost);
                 ship.ReSetPos(pos);
                 long stateNum = ship.SetShipState(RunningStateType.RunningActively, ShipStateType.Null);
                 ship.ResetShipState(stateNum);
@@ -54,7 +55,7 @@ namespace Gaming
             public void BeAttacked(Ship ship, Bullet bullet)
             {
                 Debugger.Output(ship, " is attacked!");
-                Debugger.Output(bullet, " 's AP is " + bullet.AP.ToString());
+                Debugger.Output(bullet, $" 's AP is {bullet.AP}");
                 if (bullet!.Parent!.TeamID == ship.TeamID)
                 {
                     return;
@@ -63,31 +64,31 @@ namespace Gaming
                 if (bullet.TypeOfBullet != BulletType.Missile && ship.Shield > 0)
                 {
                     ship.Shield.SubPositiveV((long)(subHP * bullet.ShieldModifier));
-                    Debugger.Output(ship, " 's shield is " + ship.Shield.ToString());
+                    Debugger.Output(ship, $" 's shield is {ship.Shield}");
                 }
                 else if (ship.Armor > 0)
                 {
                     ship.Armor.SubPositiveV((long)(subHP * bullet.ArmorModifier));
-                    Debugger.Output(ship, " 's armor is " + ship.Armor.ToString());
+                    Debugger.Output(ship, $" 's armor is {ship.Armor}");
                 }
                 else
                 {
                     ship.HP.SubPositiveV(subHP);
-                    Debugger.Output(ship, " 's HP is " + ship.HP.ToString());
+                    Debugger.Output(ship, $" 's HP is {ship.HP}");
                 }
                 if (ship.HP == 0)
                 {
                     Debugger.Output(ship, " is destroyed!");
                     var money = ship.GetCost();
                     bullet.Parent.AddMoney(money);
-                    Debugger.Output(bullet.Parent, " get " + money.ToString() + " money because of destroying " + ship);
+                    Debugger.Output(bullet.Parent, $" get {money} money because of destroying {ship}");
                     Remove(ship);
                 }
             }
             public void BeAttacked(Ship ship, long AP, long teamID)
             {
                 Debugger.Output(ship, " is attacked!");
-                Debugger.Output("AP is " + AP.ToString());
+                Debugger.Output($"AP is {AP}");
                 if (AP <= 0)
                 {
                     return;
@@ -95,19 +96,19 @@ namespace Gaming
                 if (ship.Armor > 0)
                 {
                     ship.Armor.SubPositiveV(AP);
-                    Debugger.Output(ship, " 's armor is " + ship.Armor.ToString());
+                    Debugger.Output(ship, $" 's armor is {ship.Armor}");
                 }
                 else
                 {
                     ship.HP.SubPositiveV(AP);
-                    Debugger.Output(ship, " 's HP is " + ship.HP.ToString());
+                    Debugger.Output(ship, $" 's HP is {ship.HP}");
                 }
                 if (ship.HP == 0)
                 {
                     Debugger.Output(ship, " is destroyed!");
                     var money = ship.GetCost();
                     game.TeamList[(int)teamID].AddMoney(money);
-                    Debugger.Output(ship, " get " + money.ToString() + " money because of destroying " + ship);
+                    Debugger.Output(ship, $" get {money} money because of destroying {ship}");
                     Remove(ship);
                 }
             }
@@ -121,7 +122,7 @@ namespace Gaming
                 new Thread
                 (() =>
                 {
-                    Debugger.Output(ship, " is stunned for " + time.ToString() + " ms");
+                    Debugger.Output(ship, $" is stunned for {time} ms");
                     Thread.Sleep(time);
                     ship.ResetShipState(stateNum);
                 }
@@ -143,7 +144,7 @@ namespace Gaming
                 new Thread
                 (() =>
                 {
-                    Debugger.Output(ship, " is swinging for " + time.ToString() + " ms");
+                    Debugger.Output(ship, $" is swinging for {time} ms");
                     Thread.Sleep(time);
                     ship.ResetShipState(stateNum);
                 }
@@ -187,7 +188,7 @@ namespace Gaming
                 shipValue += ship.ArmorModule.Cost;
                 shipValue += ship.ShieldModule.Cost;
                 shipValue += ship.WeaponModule.Cost;
-                Debugger.Output(ship, " 's value is " + shipValue.ToString());
+                Debugger.Output(ship, $" 's value is {shipValue}");
                 ship.AddMoney((long)(shipValue * 0.5 * ship.HP / ship.HP.GetMaxV()));
                 Debugger.Output(ship, " is recycled!");
                 Remove(ship);
