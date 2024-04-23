584e6fc494f650728d5a0e341b1c38e3fa570a6a
ONLOX <3316820553@qq.com>
2024-04-23 19:38:20 +0800

    feat: :alien:

---
+++ dependency/shell/docker.sh
@@ -0,0 +1,23 @@
+
+while getopts ':cr' 'OPT'; do
+    case ${OPT} in
+        'c') #传入cpp文件并编译
+            #echo v"$VERSION"
+            cd /usr/local/PlayerCode/CAPI/cpp
+            ./compile.sh
+            exit
+            ;;
+        'r') #运行
+            #NO_PROMPT='true'
+            cd /usr/local
+            ./run_server.sh
+            ./run_client.sh
+            ;;
+        'l') 
+
+            ;;
+        'i')
+            INSTALL_VERSION="${OPTARG}"
+            ;;
+    esac
+done
