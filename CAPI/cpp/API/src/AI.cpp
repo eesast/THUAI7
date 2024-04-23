9da9307f1a6db6605d0b5022724e963c0dcc85bd
sigureling <1678348940@qq.com>
2024-04-24 00:29:02 +0800

    style: :fire: Clear remained debug code in AI.cpp and AI.py

--- CAPI/cpp/API/src/AI.cpp
+++ CAPI/cpp/API/src/AI.cpp
@@ -6,12 +6,12 @@
 #include "constants.h"
 // 注意不要使用conio.h，Windows.h等非标准库
 // 为假则play()期间确保游戏状态不更新，为真则只保证游戏状态在调用相关方法时不更新，大致一帧更新一次
-extern const bool asynchronous = true;
+extern const bool asynchronous = false;
 
 // 选手需要依次将player1到player4的船类型在这里定义
 extern const std::array<THUAI7::ShipType, 4> ShipTypeDict = {
-    THUAI7::ShipType::MilitaryShip,
     THUAI7::ShipType::CivilianShip,
+    THUAI7::ShipType::MilitaryShip,
     THUAI7::ShipType::MilitaryShip,
     THUAI7::ShipType::FlagShip,
 };
@@ -25,17 +25,6 @@ void AI::play(IShipAPI& api)
     }
     else if (this->playerID == 2)
     {
-        while (true)
-        {
-            api.MoveDown(100);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            api.MoveLeft(100);
-            api.PrintSelfInfo();
-            api.PrintShip();
-            api.Recover(10);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            api.Attack(1.5);
-        }
     }
 
     else if (this->playerID == 3)
@@ -43,15 +32,9 @@ void AI::play(IShipAPI& api)
     }
     else if (this->playerID == 4)
     {
-        api.MoveDown(100);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        api.MoveLeft(100);
     }
 }
 
 void AI::play(ITeamAPI& api)  // 默认team playerID 为0
 {
-    api.PrintSelfInfo();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    api.InstallModule(2, THUAI7::ModuleType::ModuleLaserGun);
 }
