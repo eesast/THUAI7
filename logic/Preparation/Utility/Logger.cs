d2544487014644027138985454fcc59d5097426a
964293341 <asdawej123@163.com>
2024-04-25 12:39:31 +0800

    feat: :sparkles: a more powerful logger

--- logic/Preparation/Utility/Logger.cs
+++ logic/Preparation/Utility/Logger.cs
@@ -1,21 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Timothy.FrameRateTask;
 
-namespace Preparation.Utility
+namespace Preparation.Utility.Logging;
+
+public struct LogInfo
 {
-    public class Logger
+    public string FileName;
+    public string Info;
+}
+
+public class LogQueue
+{
+    public static LogQueue Global { get; } = new();
+    private static readonly object queueLock = new();
+
+    private readonly Queue<LogInfo> logInfoQueue = new();
+
+    public async Task Commit(LogInfo logInfo)
     {
-        static public void Writelog(object current, string str)
+        await Task.Run(() =>
         {
-            string path = "log.txt";
-            string log = $"[{DateTime.Now}] {current.GetType()} {current} {str}";
-            File.AppendAllText(path, log + Environment.NewLine);
-        }
-        static public void Writelog(string str)
+            lock (queueLock) logInfoQueue.Enqueue(logInfo);
+        });
+    }
+
+    private LogQueue()
+    {
+        new Thread(() =>
         {
-            string path = "log.txt";
-            string log = $"[{DateTime.Now}] {str}";
-            File.AppendAllText(path, log + Environment.NewLine);
-        }
+            new FrameRateTaskExecutor<int>(
+                loopCondition: () => Global != null,
+                loopToDo: () =>
+                {
+                    lock (queueLock)
+                    {
+                        while (logInfoQueue.Count != 0)
+                        {
+                            var logInfo = logInfoQueue.Dequeue();
+                            File.AppendAllText(logInfo.FileName, logInfo.Info + Environment.NewLine);
+                        }
+                    }
+                },
+                timeInterval: 200,
+                finallyReturn: () => 0
+                ).Start();
+        })
+        { IsBackground = true }.Start();
+    }
+}
+
+public class Logger(string module, string file)
+{
+    public void ConsoleLog(string msg, bool Duplicate = false)
+    {
+        var info = $"[{module}]{msg}";
+        Console.WriteLine(info);
+        if (Duplicate)
+            _ = LogQueue.Global.Commit(new()
+            {
+                FileName = file,
+                Info = info
+            });
+    }
+    public void ConsoleLogDebug(string msg, bool Duplicate = false)
+    {
+#if DEBUG
+        var info = $"[{module}]{msg}";
+        Console.WriteLine(info);
+        if (Duplicate)
+            _ = LogQueue.Global.Commit(new()
+            {
+                FileName = file,
+                Info = info
+            });
+#endif
+    }
+    public static string TypeName(object obj)
+    {
+        return obj.GetType().Name;
+    }
+    public static string ObjInfo(object obj, string msg)
+    {
+        return $"<{TypeName(obj)} {msg}>";
     }
 }
