7c09a13b43a820c7d965bdb329c64eff277a0a9e
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2024-05-11 02:05:43 +0800

    fix: 🐛 Fix an unexpected bug.

--- dependency/deploy/Program.cs
+++ dependency/deploy/Program.cs
@@ -13,7 +13,7 @@ Cloud.UpdateSecret(args[0], args[1]);
 Downloader d = new Downloader();
 d.Cloud.UpdateSecret(args[0], args[1]);
 d.Data.Config.InstallPath = @"D:\a\mirror\";
-d.Data.MD5DataPath = @"D:\a\mirror\hash.json";
+
 d.Log.Partner.Add(Log);
 // 每次更新需要更新默认值
 d.CurrentVersion = new TVersion();
@@ -49,7 +49,7 @@ else
 }
 
 d.Data.SaveMD5Data();
-Cloud.UploadFile(d.Data.Config.MD5DataPath, "hash.json");
+Cloud.UploadFile(d.Data.MD5DataPath, "hash.json");
 
 Cloud.UploadFile(Path.Combine(d.Data.Config.InstallPath, "CAPI", "cpp", "API", "src", "AI.cpp"),
     $"Templates/t.{d.CurrentVersion.TemplateVersion}.cpp");
