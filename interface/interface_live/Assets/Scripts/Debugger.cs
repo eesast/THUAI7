dd1d7b70dba871e859b2cd9fe642e27fed1cb9d9
hhzzff <2070620600@qq.com>
2024-05-15 16:41:25 +0800

    feat: :sparkles: updated live

--- interface/interface_live/Assets/Scripts/Debugger.cs
+++ interface/interface_live/Assets/Scripts/Debugger.cs
@@ -8,63 +8,157 @@ public class Debugger : SingletonMono<Debugger>
 {
     public string info;
     public TextMeshProUGUI text;
-    public MessageOfShip messageOfShip;
-    public MessageOfObj messageOfObj;
+    public MessageOfShip messageOfShip1, messageOfShip2;
+    public MessageOfObj messageOfObj1, messageOfObj2;
     public MessageToClient messageToClient;
     public MessageOfAll messageOfAll;
-
+    public int shipx = 1000;
     void Start()
     {
-        messageOfShip = new MessageOfShip()
-        {
-            X = 1145,
-            Y = 1919,
-            Speed = 0,
-            Hp = 0,
-            Armor = 0,
-            Shield = 0,
-            TeamId = 0,
-            PlayerId = 1,
-            Guid = 0,
-            ShipState = 0,
-            ShipType = 0,
-            ViewRange = 0,
-            ProducerType = 0,
-            ConstructorType = 0,
-            ArmorType = 0,
-            ShieldType = 0,
-            WeaponType = 0,
-            FacingDirection = 0.0
-        };
+        // messageOfShip1 = new MessageOfShip()
+        // {
+        //     X = 1145,
+        //     Y = 1919,
+        //     Speed = 0,
+        //     Hp = 0,
+        //     Armor = 0,
+        //     Shield = 0,
+        //     TeamId = 0,
+        //     PlayerId = 1,
+        //     Guid = 0,
+        //     ShipState = 0,
+        //     ShipType = 0,
+        //     ViewRange = 0,
+        //     ProducerType = 0,
+        //     ConstructorType = 0,
+        //     ArmorType = 0,
+        //     ShieldType = 0,
+        //     WeaponType = 0,
+        //     FacingDirection = 0.0
+        // };
+        // messageOfShip2 = new MessageOfShip()
+        // {
+        //     X = 1919,
+        //     Y = 19190,
+        //     Speed = 0,
+        //     Hp = 0,
+        //     Armor = 0,
+        //     Shield = 0,
+        //     TeamId = 0,
+        //     PlayerId = 1,
+        //     Guid = 0,
+        //     ShipState = 0,
+        //     ShipType = 0,
+        //     ViewRange = 0,
+        //     ProducerType = 0,
+        //     ConstructorType = 0,
+        //     ArmorType = 0,
+        //     ShieldType = 0,
+        //     WeaponType = 0,
+        //     FacingDirection = 0.0
+        // };
 
-        messageOfObj = new MessageOfObj()
-        {
-            ShipMessage = messageOfShip,
-        };
+        // messageOfObj1 = new MessageOfObj()
+        // {
+        //     ShipMessage = messageOfShip1,
+        // };
+        // messageOfObj2 = new MessageOfObj()
+        // {
+        //     ShipMessage = messageOfShip2,
+        // };
 
-        messageToClient = new MessageToClient();
-        messageOfAll = new MessageOfAll()
+        // messageToClient = new MessageToClient();
+        // messageOfAll = new MessageOfAll()
+        // {
+
+        //     GameTime = 1,
+        //     RedTeamScore = 2,
+        //     BlueTeamScore = 3,
+        //     RedTeamEnergy = 4,
+        //     BlueTeamEnergy = 5,
+        //     RedHomeHp = 6,
+        //     BlueHomeHp = 7,
+        // };
+        // messageToClient.ObjMessage.Add(messageOfObj1);
+        // messageToClient.ObjMessage.Add(messageOfObj2);
+        // messageToClient.GameState = GameState.GameRunning;
+        // messageToClient.AllMessage = messageOfAll;
+        // info = JsonConvert.SerializeObject(messageToClient);
+        // Debug.Log("myjson:" + info.ToString());
+
+
+        // MessageOfNews messageOfNews = new MessageOfNews()
+        // {
+
+        // };
+        // messageOfNews.NewsCase
+
+
+        info = System.IO.File.ReadAllText(@"D:\SoftwareDepartment\THUAI7\THUAI7\interface\interface_live\Assets\message(1).json");
+        UpdateManager.GetInstance().UpdateMessageByJson(info);
+        info = System.IO.File.ReadAllText(@"D:\SoftwareDepartment\THUAI7\THUAI7\interface\interface_live\Assets\message.json");
+        MessageOfFort messageOfFort = new MessageOfFort()
+        {
+            X = 45,
+            Y = 50,
+            Hp = 100,
+            TeamId = 1,
+        };
+        MessageOfObj messageOfObj = new MessageOfObj()
+        {
+            FortMessage = messageOfFort,
+        };
+        messageToClient = JsonConvert.DeserializeObject<MessageToClient>(info, new JsonSerializerSettings
         {
 
-            GameTime = 1,
-            RedTeamScore = 2,
-            BlueTeamScore = 3,
-            RedTeamEnergy = 4,
-            BlueTeamEnergy = 5,
-            RedHomeHp = 6,
-            BlueHomeHp = 7,
-        };
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+        });
         messageToClient.ObjMessage.Add(messageOfObj);
-        messageToClient.GameState = GameState.GameRunning;
-        messageToClient.AllMessage = messageOfAll;
         info = JsonConvert.SerializeObject(messageToClient);
         UpdateManager.GetInstance().UpdateMessageByJson(info);
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log(info.Contains(@"""shipMessage"": {
+        // ""x"": " + shipx.ToString()));
+        // info = info.Replace(@"""shipMessage"": {
+        //         ""x"": " + shipx.ToString(),
+        //         @"""shipMessage"": {
+        //         ""x"": " + ((shipx + 10)).ToString());
+        // shipx = ((shipx + 10));
+        // Debug.Log(info);
+
+        // info = System.IO.File.ReadAllText(@"D:\SoftwareDepartment\THUAI7\THUAI7\interface\interface_live\Assets\message.json");
+        // UpdateManager.GetInstance().UpdateMessageByJson(info);
+
+        // info = info.Replace(@"""shipMessage"": {
+        //         ""x"": " + shipx.ToString(),
+        //         @"""shipMessage"": {
+        //         ""x"": " + ((shipx + 10)).ToString());
+        // shipx = ((shipx + 10));
+        // UpdateManager.GetInstance().UpdateMessageByJson(info);
+        // info = info.Replace(@"""shipMessage"": {
+        //         ""x"": " + shipx.ToString(),
+        //         @"""shipMessage"": {
+        //         ""x"": " + ((shipx + 10)).ToString());
+        // shipx = ((shipx + 10));
+        // UpdateManager.GetInstance().UpdateMessageByJson(info);
+        // info = info.Replace(@"""shipMessage"": {
+        //         ""x"": " + shipx.ToString(),
+        //         @"""shipMessage"": {
+        //         ""x"": " + ((shipx + 10)).ToString());
+        // shipx = ((shipx + 10));
+        // UpdateManager.GetInstance().UpdateMessageByJson(info);
+        // info = info.Replace(@"""shipMessage"": {
+        //         ""x"": " + shipx.ToString(),
+        //         @"""shipMessage"": {
+        //         ""x"": " + ((shipx + 10)).ToString());
+        // shipx = ((shipx + 10));
+        // UpdateManager.GetInstance().UpdateMessageByJson(info);
+        // Debug.Log(info);
         // messageToClient.AllMessage.GameTime++;
         // info = JsonConvert.SerializeObject(messageToClient);
         // UpdateManager.GetInstance().UpdateMessageByJson(info);
--- interface/interface_live/Assets/Scripts/Define/Parameter/CoreParam.cs
+++ interface/interface_live/Assets/Scripts/Define/Parameter/CoreParam.cs
@@ -6,40 +6,69 @@ using UnityEngine;
 
 public class CoreParam
 {
+    // public class FrameQueue<T>
+    // {
+    //     public FrameQueue(int _maxSize = 20)
+    //     {
+    //         maxSize = _maxSize;
+    //         tail = maxSize - 1;
+    //         valQueue = new T[maxSize];
+    //     }
+    //     public readonly int maxSize;
+    //     private T[] valQueue;
+    //     private int tail;
+    //     public void Add(T val)
+    //     {
+    //         tail = (tail + 1) % maxSize;
+    //         valQueue[tail] = val;
+    //     }
+    //     public T GetValue(int index)
+    //     {
+    //         if (index >= maxSize)
+    //             return default;
+    //         return valQueue[(tail + maxSize - index) % maxSize];
+    //     }
+    // };
     public class FrameQueue<T>
     {
-        public FrameQueue(int _maxSize = 10)
+        public FrameQueue()
         {
-            maxSize = _maxSize;
-            tail = maxSize - 1;
-            valQueue = new T[maxSize];
+            valQueue = new Queue<T>();
         }
-        public readonly int maxSize;
-        private T[] valQueue;
-        private int tail;
+        private Queue<T> valQueue;
         public void Add(T val)
         {
-            tail = (tail + 1) % maxSize;
-            valQueue[tail] = val;
+            valQueue.Enqueue(val);
         }
-        public T GetValue(int index)
+        public T GetValue()
         {
-            if (index >= maxSize)
+            if (valQueue.Count == 0)
                 return default;
-            return valQueue[(tail + maxSize - index) % maxSize];
+            return valQueue.Dequeue();
+        }
+        public int GetSize()
+        {
+            return valQueue.Count;
         }
     };
+    // public static FrameQueue<string> stringQueue = new FrameQueue<string>();
     public static FrameQueue<MessageToClient> frameQueue = new FrameQueue<MessageToClient>();
+    public static MessageToClient firstFrame, currentFrame;
     public static MessageOfMap map;
-    public static Dictionary<Tuple<int, int>, MessageOfResource> resources;
+    public static Dictionary<Tuple<int, int>, MessageOfResource> resources = new Dictionary<Tuple<int, int>, MessageOfResource>();
     public static MessageOfHome[] homes = new MessageOfHome[2];
     public static MessageOfTeam[] teams = new MessageOfTeam[2];
-    public static MessageOfShip[] ships = new MessageOfShip[8];
-    public static GameObject[] shipsG = new GameObject[8];
-    public static Dictionary<long, MessageOfBullet> bullets;
-    public static Dictionary<long, MessageOfBombedBullet> bombedBullets;
-    public static List<MessageOfFactory> factories;
-    public static List<MessageOfCommunity> communities;
-    public static List<MessageOfFort> forts;
+    public static Dictionary<long, MessageOfShip> ships = new Dictionary<long, MessageOfShip>();
+    public static Dictionary<long, GameObject> shipsG = new Dictionary<long, GameObject>();
+    public static Dictionary<long, MessageOfBullet> bullets = new Dictionary<long, MessageOfBullet>();
+    public static Dictionary<long, GameObject> bulletsG = new Dictionary<long, GameObject>();
+    public static Dictionary<Tuple<int, int>, MessageOfFactory> factories = new Dictionary<Tuple<int, int>, MessageOfFactory>();
+    public static Dictionary<Tuple<int, int>, GameObject> factoriesG = new Dictionary<Tuple<int, int>, GameObject>();
+    public static Dictionary<Tuple<int, int>, MessageOfCommunity> communities = new Dictionary<Tuple<int, int>, MessageOfCommunity>();
+    public static Dictionary<Tuple<int, int>, GameObject> communitiesG = new Dictionary<Tuple<int, int>, GameObject>();
+    public static Dictionary<Tuple<int, int>, MessageOfFort> forts = new Dictionary<Tuple<int, int>, MessageOfFort>();
+    public static Dictionary<Tuple<int, int>, GameObject> fortsG = new Dictionary<Tuple<int, int>, GameObject>();
+    public static Dictionary<Tuple<int, int>, MessageOfWormhole> wormholes = new Dictionary<Tuple<int, int>, MessageOfWormhole>();
     public static bool initialized;
+    public static int cnt = 0;
 }
